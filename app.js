3edbda190f2189009c4b169078ccd6e160213d57
Luciana <52759619+ciana-banana@users.noreply.github.com>
2022-10-13 13:12:56 +0200

    working on responsive menu

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menuLinks = document.querySelector('.navbar_link_container');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
